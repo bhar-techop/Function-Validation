bd4865981c1ae3b4b1b54aec8709dd57bedd866b
bhar-techop <bhargavprabhu98@gmail.com>
2024-12-09 13:28:44 +0530

    first commit

---
+++ 01-CRUD-SIMPLE/assets/js/script.js
@@ -0,0 +1,6 @@
+let fullName=document.getElementById('fullName')
+let empCode=document.getElementById('empCode')
+let salary=document.getElementById('salary')
+let city=document.getElementById('city')
+let span=document.getElementById('fullNameValidationError')
+let employeeList=document.getElementById('employeeList')
