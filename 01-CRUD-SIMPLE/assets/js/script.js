12509a7b2b6880842b59287c88c209dbac6d0038
bhar-techop <bhargavprabhu98@gmail.com>
2024-12-10 12:05:40 +0530

    project almost completed

--- 01-CRUD-SIMPLE/assets/js/script.js
+++ 01-CRUD-SIMPLE/assets/js/script.js
@@ -1,6 +1,49 @@
-let fullName=document.getElementById('fullName')
-let empCode=document.getElementById('empCode')
-let salary=document.getElementById('salary')
-let city=document.getElementById('city')
-let span=document.getElementById('fullNameValidationError')
-let employeeList=document.getElementById('employeeList')
+let fullName = document.getElementById('fullName');
+let empCode = document.getElementById('empCode');
+let salary = document.getElementById('salary');
+let city = document.getElementById('city');
+let span = document.getElementById('fullNameValidationError');
+let employeeList = document.getElementById('employeeList');
+
+function validateFullName() {
+    let isValidate = true;
+
+    
+    if (fullName.value=== '') {
+        isValidate = false;
+        span.classList.remove("hide");
+    } else {
+      
+        if (!span.classList.contains("hide")) {
+            isValidate = true;
+            span.classList.add("hide");
+        }
+    }
+
+    return isValidate;
+}
+
+function submitHandler(event) {
+    event.preventDefault();
+    if (validateFullName()) { 
+        alert("Validated");
+    } else {
+        alert("Not validated");
+    }
+}
+//read the data from the form field
+function readFormData(){ 
+    // key:value=>property
+    let formData={}
+    formData.fullName=fullName.value
+    formData["empCode"]=empCode.value
+    formData["salary"]=salary.value
+    formData.city=city.value
+
+    //we want to clear the form it contains previous data
+    formData.fullName=""
+    formData.empCode=""
+    formData.salary=""
+    formData.city=""
+    return formData
+}
